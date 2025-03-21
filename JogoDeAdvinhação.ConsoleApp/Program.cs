44f95df4d5c2a9713c0f76c348eb138f2e895d63
caues <caues@SCHMITZNOT>
2025-03-21 14:44:02 -0300

    Implementada versão 1 com entrada e saida de dados

--- JogoDeAdvinhação.ConsoleApp/Program.cs
+++ JogoDeAdvinhação.ConsoleApp/Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
+        //  Versão 1: Estrutura básica e entrada do usuário
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Jogo de adivinhação");
+            Console.WriteLine("-------------------------------------------");
+
+
+            //Lógica do jogo
+            Console.Write("Digite um número para chutar: ");
+            int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Você digitou o número: " + numeroDigitado);
+
+
+            Console.ReadLine();
         }
     }
 }
